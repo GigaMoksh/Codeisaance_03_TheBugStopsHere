489f95b68b281a0616a74ecc264770f9d9b028b5
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-09-23 10:26:51 +0530

    intitial setup

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='bg-red text-red'>
+      <h1 className='mt-12 text-2xl'>Hello world</h1>
+    </div>
+  )
+}
