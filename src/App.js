60bf52811b3056decd597be58ce46285b7ac4dfc
Prateek Vishwakarma <prateekv2003@gmail.com>
2022-09-24 06:02:41 +0530

    404

--- src/App.js
+++ src/App.js
@@ -12,33 +12,50 @@ import Verify from "./components/Verify";
 import VerifyDocument from "./components/VerifyDocument";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useContext } from "react";
+import Four04 from './components/Four04'
 const App = () => {
 
   useEffect(() => {
     AOS.init();
   }, []);
+
+  const isauth=true;
   return (
     <MoralisProvider
       serverUrl="https://9srnar891yfo.usemoralis.com:2053/server"
       appId="BJ2TKeyH7s7ywXf8ZGXwWxAhZkF9vV7mnlr5FW1o"
     >
       <AppProvider>
+      {
+        isauth
+        ?
+        <Router>
+          <div>
+            <Routes>
+              <Route exact path="/" element={<UserDashboard />} />
+              <Route exact path="/login" element={<h1>vaibhav</h1>} />
+              <Route exact path="/recovery-password" element={<h1>Login</h1>} />
+              <Route exact path="/admin" element={<Admin />} />
+              <Route exact path="/upload-doc" element={<UploadDoc />} />
+              <Route exact path="verify" element={<Verify />} />
+              <Route exact path="verifyDoc" element={<VerifyDocument />} />
+              <Route exact path="*" element={<Four04/>} />
+            </Routes>
+          </div>
+        </Router>
+        :
         <Router>
           <div>
             {/* <Home /> */}
             <Routes>
               <Route exact path="/" element={<Home />} />
-              <Route exact path="/userDashboard" element={<UserDashboard />} />
-              <Route exact path="/login" element={<h1>vaibhav</h1>} />
-              <Route exact path="/recovery-password" element={<h1>Login</h1>} />
-              <Route path="*" element={<h1>404, Not Found!</h1>} />
-              <Route exact path="/admin" element={<Admin />} />
-              <Route exact path="/upload-doc" element={<UploadDoc />} />
-              <Route exact path="verify" element={<Verify />} />
-              <Route exact path="verifyDoc" element={<VerifyDocument />} />
+              <Route exact path="*" element={<Four04/>} />
             </Routes>
           </div>
         </Router>
+      }
+        
       </AppProvider>
     </MoralisProvider>
   );
--- src/components/home_about/Home_about.jsx
+++ src/components/home_about/Home_about.jsx
@@ -6,35 +6,40 @@ import { GiCheckedShield } from 'react-icons/gi'
 import { FaUserSecret } from 'react-icons/fa'
 const Home_about = () => {
   return (
-    <div className='grid grid-cols-3 gap-x-10 p-10 my-10 home-about h-screen place-items-center'>
-      <div className="bg-blue-50 p-8 rounded-2xl shadow-md h-fit" data-aos="fade-right" data-aos-duration="2000">
-        <div className="flex items-center justify-center">
-          <BiLockAlt className='text-[100px]'/>
+    <>
+      <div className='grid grid-cols-3 gap-x-10 p-10 my-10 home-about h-screen place-items-center'>
+        <div className="bg-blue-50 p-8 rounded-2xl shadow-md h-fit" data-aos="fade-right" data-aos-duration="2000">
+          <div className="flex items-center justify-center">
+            <BiLockAlt className='text-[100px]' />
+          </div>
+          <div className="bg-aliceblue-500 p-1 pt-4 pb-4">
+            <h1 className='pb-2 font-medium'>Encrypted</h1>
+            <p className='text-justify'>Each file is encrypted using the SHA-256 before being stored in the blockchain.</p>
+          </div>
         </div>
-        <div className="bg-aliceblue-500 p-1 pt-4 pb-4">
-          <h1 className='pb-2 font-medium'>Encrypted</h1>
-          <p className='text-justify'>Each file is encrypted using the SHA-256 before being stored in the blockchain.</p>
+        <div className="bg-blue-100 p-8 rounded-2xl shadow-md h-fit" data-aos="fade-up" data-aos-duration="2000">
+          <div className="flex items-center justify-center">
+            <GiCheckedShield className='text-[100px]' />
+          </div>
+          <div className="p-1 pt-4 pb-4">
+            <h1 className='pb-2 font-medium'>Secure</h1>
+            <p className='text-justify'>A privately maintained Blockchain ensures that youur data is always secure.</p>
+          </div>
+        </div>
+        <div className="bg-blue-200 p-8 rounded-2xl shadow-md h-fit" data-aos="fade-left" data-aos-duration="2000">
+          <div className="flex items-center justify-center">
+            <FaUserSecret className='text-[100px]' />
+          </div>
+          <div className="p-1 py-2">
+            <h1 className='pb-2 font-medium'>Confidential</h1>
+            <p className='text-justify'>No outside company or third party can access or control your files, unlike traditional cloud storage providers.</p>
+          </div>
         </div>
       </div>
-      <div className="bg-blue-100 p-8 rounded-2xl shadow-md h-fit" data-aos="fade-up" data-aos-duration="2000">
-        <div className="flex items-center justify-center">
-          <GiCheckedShield className='text-[100px]'/>
-        </div>
-        <div className="p-1 pt-4 pb-4">
-          <h1 className='pb-2 font-medium'>Secure</h1>
-          <p className='text-justify'>A privately maintained Blockchain ensures that youur data is always secure.</p>
-        </div>
-      </div>
-      <div className="bg-blue-200 p-8 rounded-2xl shadow-md h-fit" data-aos="fade-left" data-aos-duration="2000">
-        <div className="flex items-center justify-center">
-          <FaUserSecret className='text-[100px]'/>
-        </div>
-        <div className="p-1 pt-4 pb-4">
-          <h1 className='pb-2 font-medium'>Confidential</h1>
-          <p className='text-justify'>You control your private encryption keys and you own your data. No outside company or third party can access or control your files, unlike traditional cloud storage providers.</p>
-        </div>
-      </div>
-    </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="rgba(14, 15, 20, 1)" fill-opacity="1" d="M0,96L80,101.3C160,107,320,117,480,112C640,107,800,85,960,69.3C1120,53,1280,43,1360,37.3L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+</svg>
+    </>
     // <div class="container">
     //   <div class="section">
 
