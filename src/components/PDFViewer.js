cd533b846fa7374d9fb08f1f75de4acb0c846c0d
Prateek Vishwakarma <prateekv2003@gmail.com>
2022-09-24 10:59:23 +0530

    downlioad pdf

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf/dist/esm/entry.webpack5";
+import { Document, Page, pdfjs} from "react-pdf/dist/esm/entry.webpack5";
 import mypdf from "../assests/pdfs/1.pdf";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
@@ -12,8 +12,28 @@ function PDFViewer({ fileurl }) {
   }
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+
+  // const MyDoc = () => (
+  //   <Document 
+  //   file={{
+  //         // url: `https://cors-anywhere.herokuapp.com/${fileurl}`,
+  //         url: fileurl,
+  //       }}
+  //       onLoadSuccess={onDocumentLoadSuccess}
+  //       onLoadError={console.error}
+  //       className="border-2"
+  //       >
+  //     <Page className="static" width={800} pageNumber={pageNumber} />
+  //   </Document>
+  // )
+
   return (
     <div className="flex items-center justify-center flex-col">
+
+      <div className="bg-red mb-5">
+        <a className="py-2 px-4 bg-blue-500 rounded text-white" href={fileurl} target="_blank" download={true}> Download PDF</a>
+      </div>
+      
       <Document
         file={{
           // url: `https://cors-anywhere.herokuapp.com/${fileurl}`,
